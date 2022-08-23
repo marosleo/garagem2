2ed35f489550e847bef06e36f4551965880c7eb3
marosleo <marosleo1990@gmail.com>
2022-08-23 14:29:08 -0300

    garagem-marosleo

---
+++ garagem/core/views.py
@@ -0,0 +1,16 @@
+from rest_framework.viewsets import ModelViewSet
+
+from core.models import Categoria, Marca, Carro
+from core.serializers import CategoriaSerializer, MarcaSerializer, CarroSerializer
+
+class CategoriaViewSet(ModelViewSet):
+    queryset = Categoria.objects.all()
+    serializer_class = CategoriaSerializer
+
+class MarcaViewSet(ModelViewSet):
+    queryset = Marca.objects.all()
+    serializer_class = MarcaSerializer
+
+class CarroViewSet(ModelViewSet):
+    queryset = Carro.objects.all()
+    serializer_class = CarroSerializer
